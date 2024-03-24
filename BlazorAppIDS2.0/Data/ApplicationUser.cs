6ac1364f24d886869ace2d6cd88c25bd6b7d91ad
dhruv-0987 <dhruvmat1998@gmail.com>
2024-03-25 10:56:47 +1100

    initial commit

---
+++ BlazorAppIDS2.0/Data/ApplicationUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazorAppIDS2._0.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
